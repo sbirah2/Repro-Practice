a88c2f1362664b807deae416b17429e218b791b4
sbirah2 <sbirah2@studentmail.nait.ca>
2016-09-09 20:48:50 -0600

    Initial Website Creation

---
+++ Repro Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Repro_Website.Startup))]
+namespace Repro_Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
